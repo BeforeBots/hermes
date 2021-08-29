ac2018c5a51f23602efdc9ec99f3c3339d77a568
TanweerulHaque <tan.haque@gmail.com>
2021-08-30 03:22:48 +0530

    fixed FC py file

--- hermes/custom/FC.py
+++ hermes/custom/FC.py
@@ -1,15 +1,21 @@
 import tensorflow as tf
 import pandas as pd
+from collections import defaultdict
 
 
 class FileCallback(tf.keras.callbacks.Callback):
-    def __init__(self, param="", filepath=""):
+    def __init__(self, save_format=""):
         super(FileCallback, self).__init__()
-        self.param = param
+        self.save_format = save_format
+        self.dfparams = defaultdict(list)
+        self.filepath = filepath
+
+    def on_epoch_end(self, epoch, logs=None):
+        keys = list(logs.keys())
+        for i in keys:
+            self.dfparams[i].append(logs[i])
 
     def on_train_end(self, logs=None):
-        s = f"""df.to_{self.param}()"""
-        print(s)
-
-
-obj = FileCallback.on_train_end()
+        df = pd.DataFrame(self.dfparams)
+        s = f"""df.to_{self.save_format}('hardcoded.csv')"""
+        eval(s)
