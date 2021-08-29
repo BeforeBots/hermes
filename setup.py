fab21e47772ae24471b894a997c552307a01629a
Shreya <shreya.out@gmail.com>
2021-08-29 11:46:09 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Bringing custom callbacks to your phone',
+    author='BeforeBots',
+    license='MIT',
+)
